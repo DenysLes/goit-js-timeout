4d571ee5acfe47de0354410bd80ba2d8f71e901e
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-02-10 18:45:02 +0200

    clock.js DONE

--- js/clock.js
+++ js/clock.js
@@ -4,6 +4,10 @@ const year = document.querySelector(".date-year");
 const date = document.querySelector(".date");
 const digitalClock = document.querySelector('.digital-clock');
 
+const arrowSeconds = document.querySelector('.clock-seconds__arrow');
+const arrowMinutes = document.querySelector('.clock-minutes__arrow');
+const arrowHours = document.querySelector('.clock-hours__arrow');
+
 const nameOfMonth = ['Січень', 'Лютий', 'Березень', 'Квітень', 'Травень','Чевень', 'Липень', 'Серпень', 'Вересень', 'Жовтень', 'Листопад', 'Грудень'];
 
 const arrDay = ['Неділя', 'Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П"ятниця', 'Субота' ];
@@ -18,7 +22,10 @@ setInterval(() => {
     const currentMinutes = currentTime.getMinutes();
     const currentSeconds = currentTime.getSeconds();
 
-    
+    const changeSeconds = (360 / 60) * currentSeconds;
+    const changeMinutes = (360 / 60) * currentMinutes;
+    const changeHours = (360 / 12 ) * currentHour + (360 / 12 / 60 ) * currentMinutes ;
+    console.log(changeSeconds);
     const formatTime = `${currentHour.toString().padStart(2, '0')} : ${currentMinutes.toString().padStart(2, '0')} : ${currentSeconds.toString().padStart(2, '0')}`
     
    
@@ -26,7 +33,12 @@ setInterval(() => {
     date.textContent = currentDate;
     month.textContent = currentMonth;
     year.textContent = currentYear;
-    digitalClock.textContent =`Поточний час : ${formatTime}`;
+
+    digitalClock.textContent = `Поточний час : ${formatTime}`;
+    
+    arrowSeconds.style.transform = `rotate(${changeSeconds}deg)`;
+    arrowMinutes.style.transform = `rotate(${changeMinutes}deg)`;
+    arrowHours.style.transform = `rotate(${changeHours}deg`;
 }, 1000);
 
 
